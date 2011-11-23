cad01f7c1a72d3c9fbce6534ecef3aa2de1250cb
Ma Yingkui <maeikei@gmail.com>
2011-11-23 22:41:43 +0900

    ctype

--- stdc/include/ctype.h
+++ stdc/include/ctype.h
@@ -20,18 +20,18 @@
 
 __BEGIN_DECLS
 
-extern const char	*_ctype_;
-extern const short	*_tolower_tab_;
-extern const short	*_toupper_tab_;
+extern const char	*CLANG_PORT_DECL(_ctype_);
+extern const short	*CLANG_PORT_DECL(_tolower_tab_);
+extern const short	*CLANG_PORT_DECL(_toupper_tab_);
 
 static __inline int isalnum(int c)
 {
-	return (c == -1 ? 0 : ((_ctype_ + 1)[(unsigned char)c] & (_U|_L|_N)));
+	return (c == -1 ? 0 : ((CLANG_PORT_DECL(_ctype_) + 1)[(unsigned char)c] & (_U|_L|_N)));
 }
 
 static __inline int isalpha(int c)
 {
-	return (c == -1 ? 0 : ((_ctype_ + 1)[(unsigned char)c] & (_U|_L)));
+	return (c == -1 ? 0 : ((CLANG_PORT_DECL(_ctype_) + 1)[(unsigned char)c] & (_U|_L)));
 }
 static __inline int isblank(int c)
 {
@@ -40,61 +40,61 @@ static __inline int isblank(int c)
 
 static __inline int iscntrl(int c)
 {
-	return (c == -1 ? 0 : ((_ctype_ + 1)[(unsigned char)c] & _C));
+	return (c == -1 ? 0 : ((CLANG_PORT_DECL(_ctype_) + 1)[(unsigned char)c] & _C));
 }
 
 static __inline int isdigit(int c)
 {
-	return (c == -1 ? 0 : ((_ctype_ + 1)[(unsigned char)c] & _N));
+	return (c == -1 ? 0 : ((CLANG_PORT_DECL(_ctype_) + 1)[(unsigned char)c] & _N));
 }
 
 static __inline int isgraph(int c)
 {
-	return (c == -1 ? 0 : ((_ctype_ + 1)[(unsigned char)c] & (_P|_U|_L|_N)));
+	return (c == -1 ? 0 : ((CLANG_PORT_DECL(_ctype_) + 1)[(unsigned char)c] & (_P|_U|_L|_N)));
 }
 
 static __inline int islower(int c)
 {
-	return (c == -1 ? 0 : ((_ctype_ + 1)[(unsigned char)c] & _L));
+	return (c == -1 ? 0 : ((CLANG_PORT_DECL(_ctype_) + 1)[(unsigned char)c] & _L));
 }
 
 static __inline int isprint(int c)
 {
-	return (c == -1 ? 0 : ((_ctype_ + 1)[(unsigned char)c] & (_P|_U|_L|_N|_B)));
+	return (c == -1 ? 0 : ((CLANG_PORT_DECL(_ctype_) + 1)[(unsigned char)c] & (_P|_U|_L|_N|_B)));
 }
 
 static __inline int ispunct(int c)
 {
-	return (c == -1 ? 0 : ((_ctype_ + 1)[(unsigned char)c] & _P));
+	return (c == -1 ? 0 : ((CLANG_PORT_DECL(_ctype_) + 1)[(unsigned char)c] & _P));
 }
 
 static __inline int isspace(int c)
 {
-	return (c == -1 ? 0 : ((_ctype_ + 1)[(unsigned char)c] & _S));
+	return (c == -1 ? 0 : ((CLANG_PORT_DECL(_ctype_) + 1)[(unsigned char)c] & _S));
 }
 
 static __inline int isupper(int c)
 {
-	return (c == -1 ? 0 : ((_ctype_ + 1)[(unsigned char)c] & _U));
+	return (c == -1 ? 0 : ((CLANG_PORT_DECL(_ctype_) + 1)[(unsigned char)c] & _U));
 }
 
 static __inline int isxdigit(int c)
 {
-	return (c == -1 ? 0 : ((_ctype_ + 1)[(unsigned char)c] & (_N|_X)));
+	return (c == -1 ? 0 : ((CLANG_PORT_DECL(_ctype_) + 1)[(unsigned char)c] & (_N|_X)));
 }
 
 static __inline int tolower(int c)
 {
 	if ((unsigned int)c > 255)
 		return (c);
-	return ((_tolower_tab_ + 1)[c]);
+	return ((CLANG_PORT_DECL(_tolower_tab_) + 1)[c]);
 }
 
 static __inline int toupper(int c)
 {
 	if ((unsigned int)c > 255)
 		return (c);
-	return ((_toupper_tab_ + 1)[c]);
+	return ((CLANG_PORT_DECL(_toupper_tab_) + 1)[c]);
 }
 
 
